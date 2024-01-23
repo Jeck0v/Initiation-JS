1545aee51c69b6932bf215de247c1ee367ad7327
Arnaud F <149910271+Jeck0v@users.noreply.github.com>
2024-01-23 21:07:47 +0100

    Create script.js
    initiation JS: event

---
+++ script.js
@@ -0,0 +1,30 @@
+//Une fonction Js qui renvoie l'input de l'utilisateur en majuscule.
+
+/*
+<body>
+    <input type="text" id="title" value>
+    <input type="text" id="content" value>
+    <p id="content"></p>
+    
+</body>
+*/
+/*
+    const title = document.querySelector('input')
+    const content = document.querySelector('#content')
+title.addEventListener('keyup', function(e){
+    console.log(e.target.value)
+    content.textContent = e.target.value.toUpperCase()
+})
+*/
+//Écrivez un programme qui demande à l'utilisateur d'entrer un nombre. Le programme affichera ensuite si le nombre est pair ou impair.
+/*
+const title = document.querySelector('input')
+const content = document.querySelector('#content')
+title.addEventListener('keyup', function(e){
+    if (title.value % 2 === 0) {
+    console.log(title.value + " est un nombre Pair");
+} else {
+    console.log(title.value + " est un nombre Impair");
+}
+})
+*/
